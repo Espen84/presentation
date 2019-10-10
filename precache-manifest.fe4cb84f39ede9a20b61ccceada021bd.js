899d0a457fa574e2b0398941305db33c4fa2ffb1
vegaal17 <vegaal17@uia.no>
2019-10-10 22:39:16 +0200

    Updates

---
+++ precache-manifest.fe4cb84f39ede9a20b61ccceada021bd.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b2f76796b480a6bc206ba2e5cd53b8f4",
+    "url": "/presentation/index.html"
+  },
+  {
+    "revision": "6cd37ab343e2d21478ed",
+    "url": "/presentation/static/css/main.b550c907.chunk.css"
+  },
+  {
+    "revision": "ac1eb3e10d87366e6311",
+    "url": "/presentation/static/js/2.9b2476e1.chunk.js"
+  },
+  {
+    "revision": "6cd37ab343e2d21478ed",
+    "url": "/presentation/static/js/main.c03b911f.chunk.js"
+  },
+  {
+    "revision": "35452d8ff7ad1e86267c",
+    "url": "/presentation/static/js/runtime-main.8acc2b51.js"
+  },
+  {
+    "revision": "43dccebedb54a81caeb4f8b775a050d2",
+    "url": "/presentation/static/media/github.43dccebe.svg"
+  },
+  {
+    "revision": "33c2af7a1714eaa7cc208603d9e5a315",
+    "url": "/presentation/static/media/linkedin.33c2af7a.svg"
+  }
+]);
